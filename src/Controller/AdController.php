56e2734b9088fb4ca1effbe8293e06b0046d2863
RayL2590 <ry.loche@laposte.net>
2020-11-09 18:02:09 +0000

    initiation injection dependance avec adRepository dans adController

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,18 +3,21 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Repository\AdRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class AdController extends AbstractController
 {
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index(): Response
+    public function index(AdRepository $repo, SessionInterface $session): Response
     {
-        $repo = $this->getDoctrine()->getRepository(Ad::class);
+        //$repo = $this->getDoctrine()->getRepository(Ad::class);
+        dump($session);
         $ads = $repo->findAll();
         return $this->render('ad/index.html.twig', [
             'ads' => $ads,
