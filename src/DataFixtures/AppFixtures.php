4791d6e7ecef693a8c807e12786bb696c633a36a
RayL2590 <ry.loche@laposte.net>
2020-11-09 15:51:03 +0000

    zaninoto

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,20 +5,26 @@ namespace App\DataFixtures;
 use App\Entity\Ad;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = Factory::create('FR-fr');
+
         for($i=1; $i <=30; $i++){
 
             $ad = new Ad;
-    
-            $ad->setTitle("Titre de l'annonce n°$i")
-                ->setSlug("titre-de-l-annonce n°$i")
-                ->setCoverImage("http://placehol.it/1000x30")
-                ->setIntroduction("Bonjour voici l'intro")
-                ->setContent("<p>Contenu riche hein</p>")
+            $title = $faker->sentence();
+            $coverImage = $faker->imageUrl(1000,350);
+            $introduction = $faker->paragraph(2);
+            $content = '<p>' . join('</p><p>', $faker->paragraphs(5)) . '</p>'; //ouverture et fermeture de paragraphes
+            $ad->setTitle($title)
+                ->setSlug($coverImage)
+                ->setCoverImage($coverImage)
+                ->setIntroduction($introduction)
+                ->setContent($content)
                 ->setPrice(mt_rand(30, 250))
                 ->setRooms(mt_rand(1,6));
             
