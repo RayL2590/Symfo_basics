8eac430cd2d31246c2612dd7e878b04f624d3cbe
RayL2590 <ry.loche@laposte.net>
2020-11-10 13:23:01 +0000

    debut du form dans adcontroller

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -22,17 +22,39 @@ class AdController extends AbstractController
             'ads' => $ads,
         ]);
     }
+    
+    /**
+     * Permet de créer une annonce
+     * @Route("/ads/new", name="ads_create")
+     * @return Response
+     */
+    public function create(){
 
+        $ad = new Ad();
+        $form = $this->createFormBuilder($ad)
+                    ->add('title')
+                    ->add('introduction')
+                    ->add('content')
+                    ->add('rooms')
+                    ->add('price')
+                    ->add('coverImage')
+                    ->getForm();
+
+        return $this->render('ad/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
     /**
      * Permet d'afficher une seule annonce
      *@Route("/ads/{slug}", name="ads_show")
      * @return Response
      */
-    public function show($slug, AdRepository $repo){
+    public function show(Ad $ad){
         //je récupère l'annonce correspondant au slug
-        $ad = $repo->findOneBySlug($slug); //findBySlug renverrait lui un tableau
+        //$ad = $repo->findOneBySlug($slug); //findBySlug renverrait lui un tableau
         return $this->render('ad/show.html.twig', [
             'ad' => $ad
         ]);
     }
+
 }
