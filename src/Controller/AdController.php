0d4624c78e83932a7a900cdfa5e60b41aaeed24f
RayL2590 <ry.loche@laposte.net>
2020-11-10 08:01:34 +0000

    retourner une annonce dans adController et ad show

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -14,13 +14,25 @@ class AdController extends AbstractController
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index(AdRepository $repo, SessionInterface $session): Response
+    public function index(AdRepository $repo): Response
     {
         //$repo = $this->getDoctrine()->getRepository(Ad::class);
-        dump($session);
         $ads = $repo->findAll();
         return $this->render('ad/index.html.twig', [
             'ads' => $ads,
+        ]);
+    }
+
+    /**
+     * Permet d'afficher une seule annonce
+     *@Route("/ads/{slug}", name="ads_show")
+     * @return Response
+     */
+    public function show($slug, AdRepository $repo){
+        //je récupère l'annonce correspondant au slug
+        $ad = $repo->findOneBySlug($slug); //findBySlug renverrait lui un tableau
+        return $this->render('ad/show.html.twig', [
+            'ad' => $ad
         ]);
     }
 }
